c80cd80ea45b4f341dc94085d724b6964efe1fef
Matt <mattstates@icloud.com>
2018-09-09 08:00:21 -0700

    Fix eslint warnings and errors

--- mockData/sampleData1.js
+++ mockData/sampleData1.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 export default {
     expand: 'schema,names',
     startAt: 0,
--- utils/urls.js
+++ utils/urls.js
@@ -1,17 +1,10 @@
-const baseJiraApiUrl =
-    '/plugins/servlet/applinks/proxy?appId=8da36fe0-de48-3afa-aae3-970436460c88&path=https%3A%2F%2Flampstrack.lampsplus.com%3A8443%2Frest%2Fapi%2F2%2F';
-
-const baseURL =
-    'https://confluence.lampsplus.com:8093/plugins/servlet/applinks/proxy?appId=8da36fe0-de48-3afa-aae3-970436460c88&path=https%3A%2F%2Flampstrack.lampsplus.com%3A8443%2Frest%2Fapi%2F2%2F';
-
-const jiraApiUrl = baseJiraApiUrl + 'search?';
-
-const jiraApiUrlByIssue = baseURL + 'issue/';
-
-const lampstrackUrl = 'https://lampstrack.lampsplus.com:8443/browse/';
-
-const jiraAPIKey = 'YofkFYFIy5bztnbtJikY61D4';
-
+/* eslint-disable max-len */
 const base64Key = 'bXN0YXRlczpZb2ZrRllGSXk1Ynp0bmJ0SmlrWTYxRDQ=';
+const baseJiraApiUrl = '/plugins/servlet/applinks/proxy?appId=8da36fe0-de48-3afa-aae3-970436460c88&path=https%3A%2F%2Flampstrack.lampsplus.com%3A8443%2Frest%2Fapi%2F2%2F';
+const baseURL = 'https://confluence.lampsplus.com:8093/plugins/servlet/applinks/proxy?appId=8da36fe0-de48-3afa-aae3-970436460c88&path=https%3A%2F%2Flampstrack.lampsplus.com%3A8443%2Frest%2Fapi%2F2%2F';
+// const jiraAPIKey = 'YofkFYFIy5bztnbtJikY61D4';
+const jiraApiUrl = `${baseJiraApiUrl}search?`;
+const jiraApiUrlByIssue = `${baseURL}issue/`;
+const lampstrackUrl = 'https://lampstrack.lampsplus.com:8443/browse/';
 
 export { baseURL, jiraApiUrl, lampstrackUrl, jiraApiUrlByIssue, base64Key };
