462078de4b2faa5b71ce0d59a510f73b8caee32a
Matt <mattstates@icloud.com>
2019-06-20 11:41:52 -0700

    code cleanup

--- Components/Table/Status.jsx
+++ Components/Table/Status.jsx
@@ -3,8 +3,8 @@ import ReactTooltip from 'react-tooltip';
 import TimeInStatus from './TimeInStatus.jsx';
 import { ALERT_SYMBOL, FLAG_SYMBOL, KEYTASK_SYMBOL } from '../../utils/constants.js';
 
-const Status = (props) => {
-    const showTeamName = props.info.resourceGroup && props.info.resourceGroup.name;
+export default function Status({ info }) {
+    const showTeamName = info.resourceGroup && info.resourceGroup.name;
 
     const resourceGroupWarningIcon = showTeamName ? null : (
         <span className="hasTeam" title="No Team Assigned">
@@ -12,30 +12,28 @@ const Status = (props) => {
         </span>
     );
 
-    const keyTaskIcon = props.info.keyTask && (
+    const keyTaskIcon = info.keyTask && (
         <span className="keyTask" title="Key Task">
             {KEYTASK_SYMBOL}
         </span>
     );
 
-    const flagsIcon = props.info.flags && (
-        <span className="keyTask" title={props.info.flags}>
+    const flagsIcon = info.flags && (
+        <span className="keyTask" title={info.flags}>
             {FLAG_SYMBOL}
         </span>
     );
 
     return (
         <div className="status" data-tip={showTeamName}>
-            {props.info.status}
+            {info.status}
             <div className="iconRow">
                 {resourceGroupWarningIcon}
                 {keyTaskIcon}
                 {flagsIcon}
             </div>
             <ReactTooltip place="top" type="dark" effect="solid" />
-            {props.info.status && <TimeInStatus issueId={props.info.id} status={props.info.status} />}
+            {info.status && <TimeInStatus issueId={info.id} status={info.status} />}
         </div>
     );
-};
-
-export default Status;
+}
