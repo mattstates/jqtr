2dbc7231dfbaa5def1645694bae3ef9fe1a86577
Matt <mattstates@icloud.com>
2019-06-20 08:00:37 -0700

    Added tooltips, red warning text, progress percentage bar, time in status, and react hooks w/ refactors.

--- Components/SearchBar.jsx
+++ Components/SearchBar.jsx
@@ -1,15 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import TimeInStatusContext from '../contexts/TimeInStatusContext.js';
 import { storageAvailable } from '../utils/utils.js';
 
 export default function SearchBar({ initialSearchString, submitCallback, abortController }) {
     const [searchString, updateSearchString] = useState(initialSearchString);
+    // TODO: Consider removing the timeInStatusContext reset to a different component. Having the
+    // App component handle the context was causing tons of re-renders of the whole app due to context
+    // data changes...
+    const timeInStatusContext = useContext(TimeInStatusContext);
 
-    useEffect(
-        () => {
-            updateSearchString(initialSearchString);
-        },
-        [initialSearchString]
-    );
+    useEffect(() => {
+        updateSearchString(initialSearchString);
+    }, [initialSearchString]);
 
     function changeHandler(e) {
         updateSearchString(e.target.value);
@@ -24,6 +26,7 @@ export default function SearchBar({ initialSearchString, submitCallback, abortCo
         }
 
         submitCallback(searchString, abortController);
+        timeInStatusContext.clearTimeInStatus();
     }
 
     return <input type="text" value={searchString} onChange={changeHandler} onKeyUp={submit} />;
