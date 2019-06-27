269596e01973911546f0e8ee3d5701baa4873b31
Matt <mattstates@icloud.com>
2019-06-27 09:41:49 -0700

    Added tests for apiUtils and tableUtils

---
+++ Components/Table/tableUtils.test.js
@@ -0,0 +1,63 @@
+import { alphabeticalSortPinnedValue, getTooltipTimeData, sortByStatus, sortByTaskNumber, sortByTime } from './tableUtils.js';
+import { NONE } from '../../utils/constants.js';
+
+const unsortedArrayMock = [['Apple'], ['Microsoft'], ['linux'], [NONE], ['Microsofts'], ['apple'], ['12'], ['zzz']];
+
+describe('alphabeticalSortPinnedValue', () => {
+    const sortedArrayWithPinned = flattenOneLevel(unsortedArrayMock.sort((a, b) => alphabeticalSortPinnedValue(a, b, 'linux')));
+    const sortedArrayWithoutPinned = flattenOneLevel(unsortedArrayMock.sort((a, b) => alphabeticalSortPinnedValue(a, b)));
+
+    test('Sorts with pinned value at the beginning of the array.', () => {
+        const sortedPinnedArrayControl = flattenOneLevel([['linux'], ['12'], ['Apple'], ['Microsoft'], ['Microsofts'], [NONE], ['apple'], ['zzz']]);
+
+        expect(sortedArrayWithPinned).toEqual(sortedPinnedArrayControl);
+    });
+
+    test(`Sorts with default value "${NONE}" pinned value at the beginning of the array.`, () => {
+        const sortedArrayControl = flattenOneLevel([[NONE], ['12'], ['Apple'], ['Microsoft'], ['Microsofts'], ['apple'], ['linux'], ['zzz']]);
+
+        expect(sortedArrayWithoutPinned).toEqual(sortedArrayControl);
+    });
+
+    test('Uppercase is sorted before lowercase.', () => {
+        const appleUppercaseIndex = sortedArrayWithPinned.indexOf('Apple');
+        const appleLowercaseIndex = sortedArrayWithPinned.indexOf('apple');
+        expect(appleUppercaseIndex < appleLowercaseIndex).toBe(true);
+    });
+
+    test('Number is sorted before Uppercase.', () => {
+        const appleUppercaseIndex = sortedArrayWithPinned.indexOf('Apple');
+        const numberIndex = sortedArrayWithPinned.indexOf('12');
+        expect(numberIndex < appleUppercaseIndex).toBe(true);
+    });
+
+    test('Shorter string is sorted before longer string.', () => {
+        const microsoft = sortedArrayWithPinned.indexOf('Microsoft');
+        const microsofts = sortedArrayWithPinned.indexOf('Microsofts');
+        expect(microsoft < microsofts).toBe(true);
+    });
+});
+
+describe('sortByStatus', () => {
+    const unsortedMockData = ['z', 'd', 'fff', 'ff', 'FF', 'p', 'fF', '1', '11', 'EE'];
+    const sortedControlData = ['1', '11', 'd', 'EE', 'ff', 'FF', 'fF', 'fff', 'p', 'z'];
+    const sortedData = unsortedMockData.sort(sortByStatus);
+
+    test('Results should be alphabetically sorted.', () => {
+        expect(sortedData).toEqual(sortedControlData);
+    });
+
+    test('Lettercase will be ignored.', () => {
+        expect(sortedData.join(',')).toContain('ff,FF,fF');
+    });
+
+    test('Numbers sort before letters.', () => {
+        expect(sortedData[0]).toBe('1');
+    });
+});
+
+function flattenOneLevel(array) {
+    return array.reduce((collection, element) => {
+        return [...collection, element[0]];
+    }, []);
+}
--- utils/apiUtils.test.js
+++ utils/apiUtils.test.js
@@ -1,9 +1,38 @@
-import { getFetchOptions } from './apiUtils.js';
+import { gatherAllTasks, mapToUsefulData, getFetchOptions, findMissingParentTasks, mapSubtasksToParents } from './apiUtils.js';
+import sampleResponse from '../mockData/sampleData1.js';
 
-test('TEST1_DEMO', () => {
-    expect(typeof getFetchOptions).toBe('function');
+describe('mapToUsefulData', () => {
+    let data = mapToUsefulData(sampleResponse.issues[0]);
+
+    test('Mapped Data has ID property', () => {
+        expect(data).toHaveProperty('id');
+    });
+
+    test('Mapped Data has subtasks property', () => {
+        expect(data).toHaveProperty('subtasks');
+    });
+
+    test('Mapped Data .subtasks is an array', () => {
+        expect(data.subtasks).toHaveLength(0);
+    });
 });
 
-test('TEST2_DEMO', () => {
-    expect(typeof {}).toBe('object');
+describe('mapSubtasksToParents', () => {
+    let data = mapSubtasksToParents(sampleResponse.issues.map(mapToUsefulData));
+
+    test('Does not return subtasks', () => {
+        expect(data.filter((task) => task.isSubtask)).toHaveLength(0);
+    });
+
+    test('At least one parent task has a subtask array that isn\'t empty', () => {
+        expect(data.filter((task) => task.subtasks.length).length).toBeGreaterThanOrEqual(1);
+    });
+});
+
+describe('findMissingParentTasks', () => {
+    let data = sampleResponse.issues.map(mapToUsefulData);
+
+    test('Returns missing tasks', () => {
+        expect(findMissingParentTasks(data)).toHaveLength(2);
+    });
 });
