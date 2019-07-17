9312c3cdaceb2f35777eded40a1413459c63873f
Matt States <matthews@LPDOMAIN.COM>
2019-07-16 21:45:25 -0700

    update horizontal loader

--- Components/Loaders/HorizontalLoader.jsx
+++ Components/Loaders/HorizontalLoader.jsx
@@ -3,16 +3,6 @@ import './horizontalloader.scss';
 
 export default function HorizontalLoader() {
     return (
-        <div className="horizontalLoader">
-            <span className="line line-1" />
-            <span className="line line-2" />
-            <span className="line line-3" />
-            <span className="line line-4" />
-            <span className="line line-5" />
-            <span className="line line-6" />
-            <span className="line line-7" />
-            <span className="line line-8" />
-            <span className="line line-9" />
-        </div>
+        <div className="horizontalLoader" />
     );
 }
